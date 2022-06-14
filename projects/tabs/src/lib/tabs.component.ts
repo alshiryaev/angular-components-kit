8b2865f162647104e5393f515a7ae11e09b74026
Aleksei Shiriaev <alexpl_07@inbox.ru>
2022-06-14 20:03:09 +0300

    Added selectedTabIndexChanged event

--- projects/tabs/src/lib/tabs.component.ts
+++ projects/tabs/src/lib/tabs.component.ts
@@ -3,28 +3,34 @@ import {
   Component,
   ContentChildren,
   QueryList,
-} from '@angular/core'
-import {TabComponent} from './tab/tab.component'
+  Output, EventEmitter,
+} from '@angular/core';
+import {TabComponent} from './tab/tab.component';
 
 @Component({
   selector: 'sa-tabs',
   templateUrl: './tabs.component.html',
-  styleUrls: ['./tabs.components.scss']
+  styleUrls: ['./tabs.components.scss'],
 })
 export class TabsComponent implements AfterViewInit {
   tabs: TabComponent[] = [];
   activeTabIndex: number = 0;
-  @ContentChildren(TabComponent) tabComponents: QueryList<TabComponent> = new QueryList<TabComponent>()
+  @ContentChildren(TabComponent)
+  tabComponents: QueryList<TabComponent> = new QueryList<TabComponent>();
+
+  @Output()
+  readonly selectedTabIndexChanged = new EventEmitter<number>();
 
   ngAfterViewInit(): void {
-    this.tabs = this.tabComponents.toArray()
+    this.tabs = this.tabComponents.toArray();
   }
 
   tabClick(index: number): void {
-    this.selectTabByIndex(index)
+    this.selectTabByIndex(index);
   }
 
   selectTabByIndex(index: number): void {
-    this.activeTabIndex = index
+    this.activeTabIndex = index;
+    this.selectedTabIndexChanged.emit(index);
   }
 }
