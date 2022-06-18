0c767012e8e7964c3bc51b7371a51bb880d45d3c
Aleksei Shiriaev <alexpl_07@inbox.ru>
2022-06-18 19:14:36 +0300

    Slider
    * Slider buttons

--- projects/slider/src/lib/slider.component.ts
+++ projects/slider/src/lib/slider.component.ts
@@ -10,6 +10,11 @@ import {
 } from '@angular/core';
 import {SliderItemDirective} from './slider-item.directive';
 
+enum Direcition {
+  Left = -1,
+  Right = 1,
+}
+
 @Component({
   selector: 'sa-slider',
   templateUrl: 'slider.component.html',
@@ -19,11 +24,12 @@ export class SliderComponent implements AfterContentInit, OnDestroy {
   @ContentChildren(SliderItemDirective, {read: TemplateRef})
   slidersContent!: QueryList<TemplateRef<ElementRef>>;
 
-  @Input()
-  interval = 5000;
+  readonly enums = {
+    Direcition,
+  };
 
   @Input()
-  loop = false;
+  interval = 5000;
 
   activeSlideIndex = 0;
   slides: TemplateRef<ElementRef>[] = [];
@@ -33,18 +39,44 @@ export class SliderComponent implements AfterContentInit, OnDestroy {
     this.startInterval();
   }
 
-  ngOnDestroy(): void {
-    if (this.intervalId !== null) {
-      clearInterval(this.intervalId);
-    }
+  navigate(direction: Direcition): void {
+    this.stopInterval();
+    this.activeSlideIndex = this.getNextSlideIndex(
+      this.activeSlideIndex,
+      direction,
+      this.slides.length - 1,
+    );
+    this.startInterval();
   }
 
-  private intervalId : number | null = null;
+  ngOnDestroy(): void {
+    this.stopInterval();
+  }
+
+  private intervalId!: number;
 
   private startInterval(): void {
     this.intervalId = setInterval(() => {
-      const nextSlideIndex = this.activeSlideIndex + 1;
-      this.activeSlideIndex = nextSlideIndex > this.slides.length - 1 ? 0 : nextSlideIndex;
+      this.activeSlideIndex =
+        this.getNextSlideIndex(
+          this.activeSlideIndex,
+          Direcition.Right,
+          this.slides.length - 1);
     }, this.interval);
+  }
+
+  private stopInterval(): void {
+    clearInterval(this.intervalId);
+  }
+
+  private getNextSlideIndex(currentIndex: number, direction: Direcition, count: number): number {
+    const nextSlideIndex = currentIndex + direction;
+    if (nextSlideIndex > count) {
+      return 0;
+    } else if (nextSlideIndex <= 0) {
+      return count;
+    }
+
+    return nextSlideIndex;
   }
 }
